e12992efb2a40564962e9debb7beaf4ec27eafd1
naoya.abe <micropig3402@gmail.com>
2018-10-28 03:45:09 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,29 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
+
+  # get 'users/index'
+
+  # get 'users/show'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
+  # get 'toppages/index'
 
   root to: 'toppages#index'
-  # root to: 'tasks#index'
   
-   resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  #resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
 
 # Next I  make controller & view
